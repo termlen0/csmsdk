05fdb45480bfea5fa145ac0488fa802b2acab736
Ajay Chenampara <ajay.chenampara@gmail.com>
2017-02-27 09:31:02 -0500

    Updates from Sunday

--- csmsdk/csmsdk.py
+++ csmsdk/csmsdk.py
@@ -2,31 +2,35 @@
 import os
 import requests
 import xml.etree.ElementTree as etree
-from xml.etree.ElementTree import XML
+from jinja2 import Environment, FileSystemLoader
 import re
 import logging
 requests.packages.urllib3.disable_warnings()
-
-logging.basicConfig(filename="/tmp/csmsdk.log", level=logging.DEBUG)
 
 
 class LoginException(BaseException):
     pass
 
+
 class CsmException(BaseException):
     pass
+
 
 class Csm(object):
 
     '''Object instances will represent a CSM api session'''
     def __init__(self, context_name='None', username='guest', password='guest',
-                 url='None'):
-        logging.info("INFO: Initialized CSM object....")
+                 url='None', logdir="/tmp/"):
         self.context = context_name
         self.username = username
         self.password = password
         self.csm = url
         self.cookie =''
+        #Create the logdir
+        os.makedirs(logdir, exist_ok=True)
+        logging.basicConfig(filename=logdir + "csmsdk.log",
+                            level=logging.DEBUG)
+        logging.info("INFO: Initialized CSM object....")
 
     def login(self):
         path = "/nbi/login"
@@ -81,9 +85,10 @@ class Csm(object):
                 "INFO: {} successfully logged out..".format(self.username))
             return("{} successfully logged out".format(self.username))
 
+
     def showrun(self, confpath="/tmp"):
         '''Method to get the running config of the device by context name'''
-        #Check whether the config backup dir exists - Create if not
+        # Check whether the config backup dir exists - Create if not
         os.makedirs(confpath, exist_ok=True)
         config_file = confpath + self.context
         # Execute the API call
@@ -95,32 +100,164 @@ class Csm(object):
         <name>{}</name>
         </n:deviceConfigByNameRequest> """).format(self.context)
         headers = {'Content-type': 'text/xml'}
-        logging.debug("DEBUG: {} cookie, logged in".format(self.context))
+        logging.debug("DEBUG: {} cookie, logged in".format(self.cookie))
         response = self.session.post(url=self.csm + path, data=payload,
                                      headers=headers, verify=False)
         if response.status_code != 200:
             logging.debug("DEBUG: {} \n".format(response.text))
             logging.debug("DEBUG: session cookie -  {} ".format(self.cookie))
+            self.logout()
             raise CsmException(
                 "Error retriving the config : {}".format(response.status_code))
         else:
             root = etree.fromstring(response.text)
             try:
                 for child in root:
-                    if child.tag == 'fullConfig':
-                        try:
-                            with open(config_file, "+w") as f:
-                                f.write(child.text)
-                        except:
-                            logging.debug("Error writing backup to file")
+                    if child.tag == 'device':
+                        for gchild in child:
+                            if gchild.tag == 'fullConfig':
+                                try:
+                                    with open(config_file, "+w") as f:
+                                        f.write(gchild.text)
+                                except:
+                                    logging.debug(
+                                        "Error writing backup to file")
             except:
                 logging.debug("Error traversing the dom")
 
-            assert 0
-
-
-    def showacl(self, aclname='None'):
-        path = "/nbi/configservice/getPolicyConfigByName"
+    def showpolicy(self, policyname='None'):
+        """Given the policy name, return the shared policy(xml) as a string.
+        It is up to the endpoint consumer to parse it"""
+        # STEP 1: Collect the "policy type"
+        # STEP 1a: Collect the GID uing the context name
+        path = "/nbi/configservice/getDeviceListByType"
         payload = """<?xml version="1.0" encoding="UTF-8"?>
+        <n:deviceListByCapabilityRequest xmlns:n="csm">
+        <protVersion>1.0</protVersion>
+        <reqId>123</reqId>
+        <deviceCapability>firewall</deviceCapability>
+        </n:deviceListByCapabilityRequest> """
+        logging.info("INFO: Collecting the GID...")
+        headers = {'Content-type': 'text/xml'}
+        logging.debug("DEBUG: {} cookie, logged in".format(self.cookie))
+        response = self.session.post(url=self.csm + path, data=payload,
+                                     headers=headers, verify=False)
+        if response.status_code != 200:
+            logging.debug("DEBUG: {} \n".format(response.text))
+            logging.debug("DEBUG: session cookie -  {} ".format(self.cookie))
+            self.logout()
+            raise CsmException(
+                "Error retrieving the GID : {}".format(response.status_code))
+        else:
+            try:
+                root = etree.fromstring(response.text)
+                for node in root.findall('deviceId'):
+                    if node.find('deviceName').text == self.context:
+                        gid =node.find('gid').text
+                logging.info("INFO: GID identified - {}".format(gid))
+            except:
+                self.logout()
+                logging.error("ERROR: Unable to collect the gid..")
+        # STEP 1b: Collect the "policy type" uing the GID
+        path = "/nbi/configservice/getPolicyListByDeviceGID"
+        payload = ("""<?xml version="1.0" encoding="UTF-8"?>
+        <n:policyListByDeviceGIDRequest xmlns:n="csm">
+        <protVersion>1.0</protVersion>
+        <reqId>123</reqId>
+        <gid>{}</gid>
+        </n:policyListByDeviceGIDRequest>""").format(gid)
+        logging.info("INFO: Collecting the policyType...")
+        headers = {'Content-type': 'text/xml'}
+        logging.debug("DEBUG: {} cookie, logged in".format(self.cookie))
+        response = self.session.post(url=self.csm + path, data=payload,
+                                     headers=headers, verify=False)
+        if response.status_code != 200:
+            logging.debug("DEBUG: {} \n".format(response.text))
+            logging.debug("DEBUG: session cookie -  {} ".format(self.cookie))
+            self.logout()
+            raise CsmException(
+                "Error retrieving the policytype : {}".format(
+                    response.status_code))
+        else:
+            try:
+                root = etree.fromstring(response.text)
+                for node in root.findall('policyList'):
+                    for child in node:
+                        if child.find('name').text == policyname:
+                            policytype = child.find('type').text
+                logging.info("INFO: The policy type is {}".format(policytype))
+            except:
+                self.logout()
+                logging.error("ERROR: Unable to collect the policytype")
+        # STEP 2: Collect the policy info
+        path = "/nbi/configservice/getPolicyConfigByName"
+        payload = ("""<?xml version="1.0" encoding="UTF-8"?>
+        <n:policyConfigByNameRequest xmlns:n="csm">
+        <protVersion>1.0</protVersion>
+        <reqId>123</reqId>
+        <name>{}</name>
+        <policyType>{}</policyType>
+        </n:policyConfigByNameRequest>""").format(policyname, policytype)
+        logging.info("INFO: Collecting the policy details...")
+        headers = {'Content-type': 'text/xml'}
+        logging.debug("DEBUG: {} cookie, logged in".format(self.cookie))
+        response = self.session.post(url=self.csm + path, data=payload,
+                                     headers=headers, verify=False)
+        if response.status_code != 200:
+            logging.debug("DEBUG: {} \n".format(response.text))
+            logging.debug("DEBUG: session cookie -  {} ".format(self.cookie))
+            self.logout()
+            raise CsmException(
+                "Error retrieving the policytype : {}".format(
+                    response.status_code))
+        else:
+            try:
+                root = etree.fromstring(response.text)
+                logging.info("INFO: Policy collected....")
+                return etree.tostring(root.find('policy'), encoding="unicode")
+            except:
+                self.logout()
+                logging.error("ERROR: Unable to retrieve the policy")
 
-        """
+
+    def getpolicyobj(self, *args):
+        """Given the gid, return the details of the policy object.
+        It is up to the endpoint consumer to parse it"""
+        # Use Jinja2 to generate the xml payload
+        # Set up the directory to find the template
+        template_loader = FileSystemLoader(searchpath='templates')
+        template_env = Environment(loader=template_loader)
+        # Get the jinja2 xml template
+        xml_template = template_env.get_template('getpolicyobj.j2')
+        # Generate the payload
+        arg_dict = {'gids': args}
+        payload = xml_template.render(arg_dict)
+        assert 0
+        path = "/nbi/configservice/getPolicyObjectByGID"
+        payload = ("""<?xml version="1.0" encoding="UTF-8"?>
+        <n:getPolicyObjectByGID xmlns:n="csm">
+        <gid>{}</gid>
+        </n:getPolicyObjectByGID>""").format(gid)
+        logging.info("INFO: Collecting the object details...")
+        headers = {'Content-type': 'text/xml'}
+        logging.debug("DEBUG: {} cookie, logged in".format(self.cookie))
+        response = self.session.post(url=self.csm + path, data=payload,
+                                     headers=headers, verify=False)
+        if response.status_code != 200:
+            logging.debug("DEBUG: {} \n".format(response.text))
+            logging.debug("DEBUG: session cookie -  {} ".format(self.cookie))
+            self.logout()
+            raise CsmException(
+                "Error retrieving the policy details : {}".format(
+                    response.status_code))
+        else:
+            try:
+                root = etree.fromstring(response.text)
+                logging.info(
+                    "INFO:Entire Details for GID {} collected..".format(gid))
+                return etree.tostring(root.find('./policyObject'),
+                                          encoding="unicode")
+            except BaseException as e:
+                self.logout()
+                logging.error("ERROR: Unable to collect the policy details..")
+                logging.error(e)
--- tests/test_csmsdk.py
+++ tests/test_csmsdk.py
@@ -9,30 +9,47 @@ CONTEXT = "DEVfvMUSashdc01-1401"
 LOGDIR = "/tmp/csmsdklogs/"
 CONFIGDIR = "/tmp/csmsdkconf/"
 
+
 def test_login():
     ''' Validate that the user can login and logout successfully'''
-    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD)
+    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD, logdir=LOGDIR)
     resp = mycsm.login()
     assert "logged in" in resp
     resp = mycsm.logout()
     assert "logged out" in resp
 
 
-def test_acllist():
-    '''Given the access-list name, show the access-list'''
-    #CSM_FW_ACL_OUTSIDE
-    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD)
-    resp = mycsm.login()
-
-    resp = mycsm.logout()
-    assert "logged out" in resp
-
 def test_getconfig():
     """Given the context, get the running config"""
-    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD,
+    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD, logdir=LOGDIR,
                     context_name=CONTEXT)
-    resp = mycsm.login()
-    running_config = mycsm.showrun(confpath=CONFIGDIR)
+    mycsm.login()
+    mycsm.showrun(confpath=CONFIGDIR)
+    mycsm.logout()
+
+
+def test_acllist():
+    '''Given the access-list name, show the access-list'''
+    # CSM_FW_ACL_OUTSIDE
+    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD, logdir=LOGDIR,
+                    context_name=CONTEXT)
+    mycsm.login()
+    xml_resp = mycsm.showpolicy(policyname='DEV_CLOUD_TEST_POLICY')
+    mycsm.logout()
+    assert "policy" in xml_resp
+
+def test_getpolicyobj():
+    """Given the GID of a policy object, get its details"""
+    # The GID of the object is obtained by parsing the output of the showpolicy
+    # for instance.
+    # The idea behind this method, is to provide a means to validate the current
+    # values of policy objects; can be used for validation, before adding a new
+    # policy
+    mycsm = csm.Csm(url=URL, username=UNAME, password=PWORD, logdir=LOGDIR,
+                    context_name=CONTEXT)
+    mycsm.login()
+    xml_resp = mycsm.getpolicyobj('00000000-0000-0000-0000-017179872090','test')
+    assert 0
     mycsm.logout()
 
 
@@ -40,6 +57,7 @@ def test_acladd():
     '''Given the access-list name, insert an ACE to the list'''
     pass
 
+
 def test_aclremove():
-    '''Given the acl name, and tuple, remove the entry'''
+    '''Given the acl name,  ggggggggbbvvvvvvnnnnnng hvhvhtand tuple, remove the entry'''
     pass
